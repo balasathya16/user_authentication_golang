89751b0e7f776f9694e43d48c353690e184e8eef
DivyaTanwar <106358895+DivyaTanwar@users.noreply.github.com>
2023-02-19 10:47:56 -0500

    go fiber install / test

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 // installing go - new pc / dubai 
 
 package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get(path:"/", func (c *fiber.Ctx) error {
+		return c.SendString(body:"hi there")
+	})
+
+	app.Listen(addr: ":3000")
+}
