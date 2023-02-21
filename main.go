480cd8f82e37668169e46cb5009f24c4fce2f03a
DivyaTanwar <106358895+DivyaTanwar@users.noreply.github.com>
2023-02-21 13:21:48 -0500

    added mod and dependencies

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
-	app.Get(path:"/", func (c *fiber.Ctx) error {
+	app.Get(path:"/", func(c *fiber.Ctx) error {
 		return c.SendString(body:"hi there")
 	})
 
-	app.Listen(addr: ":3000")
+	app.Listen(addr: ":8000")
 }
