9aa6b789461f45b65a029d01c0d0096f1afa3383
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-03-16 17:56:34 +0530

    setup gin gonic, tested

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	initialize "userauth/init"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initialize.LoadEnvVariables()
+}
 
 func main() {
-	fmt.Println("hello")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
