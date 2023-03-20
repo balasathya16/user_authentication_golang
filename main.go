5d1f4e1525964596e8ba781e662e21329e5fba1e
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-03-20 14:09:55 +0530

    added gin

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"userauth.com/initializers"
+)
+
+func init() {
+	initializers.LoadEnv()
+}
 
 func main() {
-	fmt.Println("hello")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
