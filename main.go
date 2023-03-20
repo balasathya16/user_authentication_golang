54eb154af35b7bf4844bf235b170133def715de0
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-03-20 15:00:24 +0530

    attempted moving inside golang/src -- following convention

--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-	"userauth.com/initializers"
-)
-
-func init() {
-	initializers.LoadEnv()
-}
+import "fmt"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+	fmt.Println("yaao")
 }
