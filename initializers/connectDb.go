fb3939b9e1b431ba9d5d24f98fa86847fac7ab36
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-03-26 09:38:04 +0530

    added real creds in db

--- initializers/connectDb.go
+++ initializers/connectDb.go
@@ -6,6 +6,8 @@ import (
 )
 
 func ConnectDB() {
-	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable"
+
+	// postgres://:@/
+	dsn := "host=mouse.db.elephantsql.com user=ixwniyxv password=JRBipBbQtmMw36ZNr703S-PcV-o9Rqyq dbname=ixwniyxv port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 }
