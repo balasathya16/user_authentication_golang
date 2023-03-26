da0d9b6258d551698fcd72de22d1b8307a477a5a
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-03-26 09:40:34 +0530

    handling errors

--- initializers/connectDb.go
+++ initializers/connectDb.go
@@ -5,9 +5,16 @@ import (
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func ConnectDB() {
 
+	var err error
 	// postgres://:@/
 	dsn := "host=mouse.db.elephantsql.com user=ixwniyxv password=JRBipBbQtmMw36ZNr703S-PcV-o9Rqyq dbname=ixwniyxv port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect to db")
+	}
 }
