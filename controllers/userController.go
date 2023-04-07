b19b870433a4093c20f41a4f87dc438a286da8e8
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-04-07 16:10:02 -0400

    get email, pwd in body store in var

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,7 +1,26 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Signup(c *gin.Context) {
 	//get email
+	var body struct {
+		Email    string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "failed to read body",
+		})
+	}
+	//hash password
+
+	//create user
+
+	//respond
 }
