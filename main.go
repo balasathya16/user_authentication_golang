953a32d87df35eac4c74b71750661ef420e22549
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-04-10 17:52:55 -0400

    tested middleware

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"userauth/controllers"
 	"userauth/initializers"
+	"userauth/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,7 +18,7 @@ func main() {
 	r := gin.Default()
 	r.POST("/signup", controllers.Signup)
 	r.POST("/login", controllers.Login)
-	r.GET("/validate", controllers.Validate)
+	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
 
 	r.Run()
 }
