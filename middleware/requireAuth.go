22b4b7b16b267a6058507a4cc96aead04ab52dd0
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-04-11 20:03:29 -0400

    actual require auth skeleton

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -1,11 +1,21 @@
 package middleware
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 )
 
 func RequireAuth(c *gin.Context) {
-	fmt.Println("in middleware")
+	// get the cookie from request
+
+	//decode and validate cookie
+
+	//check expiration
+
+	// find the user with token
+
+	// attach to request
+
+	// continue
+
+	c.Next()
 }
