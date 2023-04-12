b326a1b43a449830601347a0d47a64a538e97f25
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-04-11 20:31:39 -0400

    Parsing token

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -1,14 +1,37 @@
 package middleware
 
 import (
+	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
 func RequireAuth(c *gin.Context) {
 	// get the cookie from request
 
-	//decode and validate cookie
+	tokenString, err := c.Cookie("Authorization")
 
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("SECRET")), nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println(claims["foo"], claims["nbf"])
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+
+	}
 	//check expiration
 
 	// find the user with token
