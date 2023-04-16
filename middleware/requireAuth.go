ebda90b311a55c9f9633abe22cbae33f94594fea
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-04-15 21:20:00 -0400

    decode and validate func

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -1,9 +1,12 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
 func RequireAuth(c *gin.Context) {
@@ -16,6 +19,21 @@ func RequireAuth(c *gin.Context) {
 	}
 
 	// decode & validate
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(os.Getenv("SECRET")), nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println(claims["foo"], claims["nbf"])
+	} else {
+		fmt.Println(err)
+	}
 
 	// check the exp
 
