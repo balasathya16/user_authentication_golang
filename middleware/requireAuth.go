d1f27c267944332ed2b789236ddaa69632490ee7
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-04-16 13:49:31 -0400

    fixed err logging

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -25,7 +25,7 @@ func RequireAuth(c *gin.Context) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 
 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
@@ -39,7 +39,10 @@ func RequireAuth(c *gin.Context) {
 		if float64(time.Now().Unix()) > claims["exp"].(float64) {
 			c.AbortWithStatus(http.StatusUnauthorized)
 
+		} else {
+			fmt.Println(err)
 		}
+
 		// Find the user with token in subject
 
 		var user models.User
